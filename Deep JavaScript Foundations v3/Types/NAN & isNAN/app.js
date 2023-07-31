79aa318fc41e33eaf02f5834a1df3171c5da68e1
shahid <shahidhashmiraj@gmail.com>
2023-07-31 19:10:05 +0530

    negative zero

--- Deep JavaScript Foundations v3/Types/NAN & isNAN/app.js
+++ Deep JavaScript Foundations v3/Types/NAN & isNAN/app.js
@@ -1,2 +1,38 @@
 // Special Values 
 
+// NaN is only value does not equal tp itself altest in jaaScript. 
+// NaN it's the onlyvalue that does not have what we  call the identity property 
+
+var myAge = Number("0o19"); // 19
+
+var myNextAge = Number("39"); // 39
+
+var myParrotAge = Number("n/a"); // NaN
+
+var subtract = myAge - "this is Nan"; // Nan
+
+console.log(myParrotAge == myParrotAge); // false;
+
+console.log(isNaN(myAge));  // false
+
+console.log(isNaN(myParrotAge)); // true
+
+console.log(isNaN("this is NaN"));  // true OOPS; 
+// this is not a number but is it the NaN value?
+
+// the historical reason of getting true is because for some reason the isNaN. utility coerces value to Number before it check them to be NaN
+
+
+Number.isNaN(myParrotAge); // true;
+
+Number.isNaN("this is string") // false;
+
+
+// sometime after that mistake happened in initail javaScript. they decided that's a bad idea.
+
+//  in ES6, we got a better utility the Number.isNaN utility
+
+
+
+
+// and the type of NaN is Number 🤷‍♂️
---
+++ Deep JavaScript Foundations v3/Types/Negative Zero/app.js
@@ -0,0 +1,55 @@
+var trendRate = -0;
+trendRate === -0 // true
+
+trendRate.toString(); // "0" OOPS;
+// js developer thinks  that it was a bug if you took a -0 and stringified it and got -0 in the string
+
+//toString is lies to us😕
+
+trendRate === 0  //  true; OOPS;
+//  triple equals is also lies to us 😕
+
+trendRate < 0;  //false
+
+// this too lie 
+
+trendRate > 0; // false
+
+// this too lie 
+
+// you could get -0s but you couldn't determine that you have one untill ES6 the added a utility called object.is
+
+// It just like quadruple equals ====
+
+Object.is(trendRate, 0); // true;
+Object.is(trendRate, 0) // false;
+
+
+// there is a utility to check is it negative or not 
+
+Math.sign(-3); // -1
+Math.sign(2); // 1
+Math.sign(-0); // -0 What?  It should return me -1 isnt'it? 
+Math.sign(0); // 0  How?    and this should return 1 right? 
+
+// Lets "Fix" Math.sign(..)
+function sign(v){
+    return v !== 0 ? Math.sign(v) : Object.is(v,0) ? -1 : 1;
+}
+sign(-3) // -1
+sign(3) // 1
+sign(-0) // -1
+sign(0) // 1
+
+// the Best example of -0 
+
+function formateTrend(trendRate){
+    var direction =
+    (trendRate < 0 || Object.is(trendRate,-0)) ? "⬇️" : "⬆️";
+    return `${direction} ${Math.abs(trendRate)}`;
+}
+formateTrend(-3);  // "⬇️ 0"
+formateTrend(3);     // "⬆️ 0"
+formateTrend(-0);  // "⬇️ 0"
+formateTrend(0);  // "⬆️ 0"
+
