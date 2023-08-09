a9efadf7db6f8f296d9d563423bd52c6c86785b4
shahid <shahidhashmiraj@gmail.com>
2023-08-09 22:01:08 +0530

    scope completed

--- Deep JavaScript Foundations v3/Scope/scope/app.js
+++ Deep JavaScript Foundations v3/Scope/scope/app.js
@@ -51,3 +51,38 @@ ask()
 //  function ****(parameter) -> this parameter is target
 
 // console.log(****(teacher)) -> this is source
+
+
+
+
+var teacher = "shahid hashmi";
+
+function otherClass(){
+    teacher = "hashmi"; // the varaible teacher = "shahid hashmi"  will be now "hashmi"
+    topic = "react";  //   but what about this when we asign a value to variable that not even exist first JS machine will search in        otherClass scope after in global scope instead of giving erorr it will say like "hey i did't find so i created one for you isn't it helpfull" and it called auto global. but there is catch it is creating in run time so the variable will created in run time and it will increase our run time to avoid this we use  "strict mode"
+    console.log("welcome")
+}
+otherClass(); // welcome
+
+teacher
+topic
+
+
+// lets use strict mode;
+
+var teacher = "shahid hashmi";
+
+function otherClass(){
+    "use strict"  // all thing ganna go through same process but when topic will search in global scope it will return REFERENCE ERORR
+    teacher = "hashmi"; // the varaible teacher = "shahid hashmi"  will be now "hashmi"
+    topic = "react";  
+    console.log("welcome")
+}
+otherClass(); // welcome
+
+teacher
+topic
+//  caveat of "strict mode"; inside of ES6 modules, strict mode is assume, that code in running in strict mode
+
+
+//  if we have a sourse referenced to a varaible that is undeclared it always throws a ReferenceError
