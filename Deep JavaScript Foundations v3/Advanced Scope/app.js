179b91000ad94f09815ee8841afa7bcecca30d30
shahid <shahidhashmiraj@gmail.com>
2023-08-12 01:21:12 +0530

    advanced scope

---
+++ Deep JavaScript Foundations v3/Advanced Scope/app.js
@@ -0,0 +1,19 @@
+// lexical scope
+
+// this refereed toString, this idea od scope being nested within each other and specifically the idea that a compiler, a parser, a processor is 
+// figurig on out all those scope ahead of time before being executed, is this the consept of lexical scope
+
+
+
+// lexical scopes & Dynamic scops :->
+
+// Lexical Scope (Static Scope):
+
+// 1. Lexical scope determines the scope of a variable based on its position in the source code's hierarchical structure.
+
+// 2. Scope is determined at compile time and remains fixed throughout the code's execution.
+
+// 3. Variables are accessed based on the nesting of functions or blocks in the code.
+
+// 4. Each inner scope has access to variables from its parent scope, but not vice versa.
+
---
+++ Deep JavaScript Foundations v3/Scope/scope & function expresion/app.js
@@ -0,0 +1,40 @@
+//  what is function expression 
+// when we assign a function it is powerfull tool be can use this function a argument;
+
+function teacher(){
+    // *********8
+}
+
+let myTeacher = function anotherTeacher(){
+    console.log(anotherTeacher);
+}
+console.log(teacher);
+console.log(myTeacher);
+console.log(anotherTeacher);
+
+//  the anotherTeacher is end up as marble in  at compile time
+//  function expression  put there identifyer into there the ownscope
+
+
+//  name function expression when in the statement the first word is function is called function expression
+
+//  name function expression is more better than annoumous function expresion 
+
+// 1. Reliable function self-refercence (recursion, etc);
+
+// it's help us in recursive 
+
+// 2. we make our code debuggable.
+
+// 3. our code become more documentible 
+// for example we have annoumous function when we see to it for what happening in it we could't understand that code by just see the annoumous  styntax we have see in body to understand so that way we use naming expression
+
+
+// NOTE :-> dont ever use arrow function just for it's short && it's anounomous
+
+
+// summary 
+
+// NAME Function dDeclaration  > Named Function Expresion > Anonymous Function Expression 
+
+// 1. NAME Function declaration have superiour  and its have some benifits over names function expression and   Named Function Expresion have huge benifite over Anonymous Function Expression 
